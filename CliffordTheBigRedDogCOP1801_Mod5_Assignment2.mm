670e22a5601d2020ff6589aadef6437fedcad9fa
CoderLyne <edlynep@yahoo.com>
2026-02-07 19:33:02 -0500

    Add files via upload

---
+++ CliffordTheBigRedDogCOP1801_Mod5_Assignment2.mm
@@ -0,0 +1,14 @@
+//Object literal notation uses curly braces to enclose an object's properties and values
+function Dog(name, show, species, role, personality, mySound, canTalk) {
+	this.name = Clifford;
+	this.show = Clifford the Big Red Dog;
+	this.species = Labrador Retriever;
+	this.role = Emily Elizabeth's pet Dog;
+	this.personality = A giant dog that gets in constant trouble;
+	this.mySound = Shake the entire town
+	this.canTalk = true
+	);
+};
+}
+
+	
