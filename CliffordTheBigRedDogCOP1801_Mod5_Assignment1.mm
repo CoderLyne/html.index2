0e6979dc381c280fa92fe34b37cbb621f90cb703
CoderLyne <edlynep@yahoo.com>
2026-02-07 19:34:00 -0500

    Add files via upload

---
+++ CliffordTheBigRedDogCOP1801_Mod5_Assignment1.mm
@@ -0,0 +1,19 @@
+const myDog = {
+    name: "Clifford",
+    show: "Clifford the Big Red Dog",
+    species: "Labrador Retriever",
+    role: "Emily Elizabeth's pet dog",
+    personality: "Giant troublemaker",
+    
+    mySound: "When I bark, I may shake the entire town"
+};
+
+document.write(
+    "Hello, my name is Clifford"
+    "when I bark I may shake the entire town "
+    " I starred in the TV show Clifford the Big Red Dog "
+    " My character was a Labrador Retriever "
+    " I was the pet dog of Emily Elizabeth "
+    " a giant dog that gets in constant trouble "
+);
+ 
